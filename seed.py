13e1cba958d31899d3c69c0244d067230bd6475b
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-20 16:28:57 -0400

    add: seed.py for seeding users

---
+++ seed.py
@@ -0,0 +1,30 @@
+from app import app
+from models.models import db
+from models.User import User
+from models.Experience import Experience
+from datetime import datetime
+from dateutil import relativedelta
+
+
+db.drop_all()
+db.create_all()
+
+u1 = User.signup(
+    badge_number=1,
+    email='admin@mail.com',
+    password='password',
+    first_name="test",
+    last_name="user",
+    dob=datetime(year=2000, month=1, day=1),
+    gender="Prefer not to say",
+    address="123 Cherry lane",
+    city="New York",
+    state="NY",
+    zip_code="11001",
+    phone_number="9991234567"
+)
+
+# add users to db
+db.session.add_all([u1])
+
+db.session.commit()
