d4bdac876f3621eaadab4b3dd833ee8396c1db60
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-20 21:03:32 -0400

    add: sign up route, refactor: login route

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from flask_jwt_extended import (
 )
 from models.User import User
 from forms.LoginForm import LoginForm
+from forms.SignUpForm import SignUpForm
 
 load_dotenv()
 
@@ -35,7 +36,6 @@ debug = DebugToolbarExtension(app)  # debug
 
 # from sqlalchemy.exc import IntegrityError
 
-
 @app.before_request
 def verify_jwt():
     """
@@ -45,13 +45,76 @@ def verify_jwt():
 
     verify_jwt_in_request(locations=['headers', 'cookies'], optional=True)
 
+@app.post("/signup")
+def signup():
+    """
+    Handle user signup.
+    Expecting JSON: {
+        "badge_number":"1",
+        "email":"sample@mail.com",
+        "password":"password",
+        "first_name":"sample",
+        "last_name":"user",
+        "dob":"datetime.datetime(2000, 1, 1, 0, 0)",
+        "gender":"Prefer not to say",
+        "address":"123 Cherry lane",
+        "city":"New York",
+        "state":"NY",
+        "zip_code":"11001",
+        "phone_number":"9991234567",
+        "is_student":"true",
+        "is_multilingual":"false"
+    }
+    Returns JSON: { "token": "dleoidlksd.aslkfjoiweflkfj.aldsjfoweifsldf" }
+    """
+
+    received = request.json
+
+    form = SignUpForm(csrf_enabled=False, data=received)
+
+    if form.validate_on_submit():
+        user = User.signup(
+            badge_number=received.get("badge_number"),
+            email=received.get("email"),
+            password=received.get("password"),
+            first_name=received.get("first_name"),
+            last_name=received.get("last_name"),
+            dob=received.get("dob"),
+            gender=received.get("gender"),
+            address=received.get("address"),
+            city=received.get("city"),
+            state=received.get("state"),
+            zip_code=received.get("zip_code"),
+            phone_number=received.get("phone_number"),
+            is_student=received.get("is_student"),
+            is_multilingual=received.get("is_multilingual")
+        )
+
+        try:
+            db.session.commit()
+
+            # Note: payload is stored on "sub" of token
+            token = create_access_token(identity=user.id)
+            return jsonify(token=token)
+
+        except Exception:
+            return jsonify(
+                errors="Invalid data: email or badge number already in use"
+            )
+
+    return jsonify(errors=form.errors)
+
 
 @app.post("/login")
 def login():
     """
     Handle user login.
     Expecting JSON: { "email": "mail@mail.com", "password": "mypassword" }
-    Returns JSON: { "token": "dleoidlksd.aslkfjoiweflkfj.aldsjfoweifsldf" }
+    Returns JSON:
+    - If provided valid data:
+    { "token": "dleoidlksd.aslkfjoiweflkfj.aldsjfoweifsldf" }
+    - If provided invalid data:
+    { "errors": "Invalid credentials" }
     """
 
     received = request.json
@@ -60,13 +123,14 @@ def login():
 
     if form.validate_on_submit():
         user = User.authenticate(
-            email=request.json.get("email"),
-            password=request.json.get("password")
+            email=received.get("email"),
+            password=received.get("password")
         )
 
-        # Note: payload is stored on "sub" of token
-        token = create_access_token(identity=user.id)
-        return jsonify(token=token)
+        if user:
+            # Note: payload is stored on "sub" of token
+            token = create_access_token(identity=user.id)
+            return jsonify(token=token)
 
-    else:
-        return jsonify(errors=form.errors)
+
+    return jsonify(errors="Invalid credentials")
