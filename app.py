d391e550beaf6983eb3b6397b66974c88268a550
Alyssa Denary <alyssa.denary@gmail.com>
2023-05-27 18:55:16 -0400

    refactor: auth routes
    Use blueprint to separate auth routes into own file with auth in url. Refactor tests to include updated url

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from models.models import db, connect_db
+from models.models import connect_db
 from flask_cors import CORS
 from flask_jwt_extended import (
-    create_access_token,
     verify_jwt_in_request,
-    current_user,
-    jwt_required,
     JWTManager
 )
 from models.User import User
-from forms.LoginForm import LoginForm
-from forms.SignUpForm import SignUpForm
 from routes.users import users
+from routes.auth import auth
 
 load_dotenv()
 
@@ -62,93 +58,6 @@ def verify_jwt():
         return jsonify(errors="Invalid token"), 401
 
 
+app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(users, url_prefix="/users")
 
-@app.post("/signup")
-def signup():
-    """
-    Handle user signup.
-    Expecting JSON: {
-        "badge_number":"1",
-        "email":"sample@mail.com",
-        "password":"password",
-        "first_name":"sample",
-        "last_name":"user",
-        "dob":"datetime.datetime(2000, 1, 1, 0, 0)",
-        "gender":"Prefer not to say",
-        "address":"123 Cherry lane",
-        "city":"New York",
-        "state":"NY",
-        "zip_code":"11001",
-        "phone_number":"9991234567",
-        "is_student":"true",
-        "is_multilingual":"false"
-    }
-    Returns JSON: { "token": "dleoidlksd.aslkfjoiweflkfj.aldsjfoweifsldf" }
-    """
-
-    received = request.json
-
-    form = SignUpForm(csrf_enabled=False, data=received)
-
-    if form.validate_on_submit():
-        user = User.signup(
-            badge_number=received.get("badge_number"),
-            email=received.get("email"),
-            password=received.get("password"),
-            first_name=received.get("first_name"),
-            last_name=received.get("last_name"),
-            dob=received.get("dob"),
-            gender=received.get("gender"),
-            address=received.get("address"),
-            city=received.get("city"),
-            state=received.get("state"),
-            zip_code=received.get("zip_code"),
-            phone_number=received.get("phone_number"),
-            is_student=received.get("is_student"),
-            is_multilingual=received.get("is_multilingual")
-        )
-
-        try:
-            db.session.commit()
-
-            # Note: payload is stored on "sub" of token
-            token = create_access_token(identity=user)
-            return jsonify(token=token)
-
-        except Exception:
-            return jsonify(
-                errors="Invalid data: email or badge number already in use"
-            )
-
-    return jsonify(errors=form.errors)
-
-
-@app.post("/login")
-def login():
-    """
-    Handle user login.
-    Expecting JSON: { "email": "mail@mail.com", "password": "mypassword" }
-    Returns JSON:
-    - If provided valid data:
-    { "token": "dleoidlksd.aslkfjoiweflkfj.aldsjfoweifsldf" }
-    - If provided invalid data:
-    { "errors": "Invalid credentials" }
-    """
-
-    received = request.json
-
-    form = LoginForm(csrf_enabled=False, data=received)
-
-    if form.validate_on_submit():
-        user = User.authenticate(
-            email=received.get("email"),
-            password=received.get("password")
-        )
-
-        if user:
-            # Note: payload is stored on "sub" of token
-            token = create_access_token(identity=user)
-            return jsonify(token=token)
-
-    return jsonify(errors="Invalid credentials")
---
+++ routes/test_auth_routes.py
@@ -0,0 +1,169 @@
+"""Auth Routes tests."""
+
+import os
+from unittest import TestCase
+from datetime import datetime
+from models.User import User
+from models.Experience import Experience
+from models.models import db
+
+# To use a different database for tests, set env variable.
+# Must be before app is imported
+os.environ['DATABASE_URI'] = "postgresql:///volunteer_management_test"
+
+from app import app
+
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+# Disable WTForms from using CSRF at all
+app.config['WTF_CSRF_ENABLED'] = False
+
+# Make Flask errors be real errors, rather than HTML pages with error info
+app.config['TESTING'] = True
+
+# Create tables once for all tests.
+# Data deleted & fresh test data set within each test
+db.create_all()
+Experience.__table__.drop(db.engine)
+db.drop_all()
+db.create_all()
+
+
+class AuthViewTestCase(TestCase):
+    def setUp(self):
+        User.query.delete()
+
+        u1 = User.signup(
+            badge_number=1,
+            email='u1@mail.com',
+            password='password',
+            first_name="u1",
+            last_name="test",
+            dob=datetime(year=2000, month=1, day=1),
+            gender="Prefer not to say",
+            address="1 Cherry lane",
+            city="New York",
+            state="NY",
+            zip_code="11001",
+            phone_number="9991234567",
+            is_student=True,
+            is_multilingual=False
+        )
+
+        db.session.flush()
+        db.session.commit()
+
+        self.u1_id = u1.id
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_signup_success(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/signup",
+                json={
+                    "badge_number":"2",
+                    "email":"u2@mail.com",
+                    "password":"password",
+                    "first_name":"u2",
+                    "last_name":"test",
+                    "dob":"2000-01-01 00:00:00",
+                    "gender":"Prefer not to say",
+                    "address":"2 Cherry lane",
+                    "city":"New York",
+                    "state":"NY",
+                    "zip_code":"11001",
+                    "phone_number":"9991234567",
+                    "is_student":True,
+                    "is_multilingual":False
+                })
+
+            self.assertIsInstance(resp.json["token"], str)
+
+    def test_signup_dupe_email(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/signup",
+                json={
+                    "badge_number":"3",
+                    "email":"u1@mail.com",
+                    "password":"password",
+                    "first_name":"duplicate email",
+                    "last_name":"test",
+                    "dob":"2000-01-01 00:00:00",
+                    "gender":"Prefer not to say",
+                    "address":"1 Cherry lane",
+                    "city":"New York",
+                    "state":"NY",
+                    "zip_code":"11001",
+                    "phone_number":"9991234567",
+                    "is_student": True,
+                    "is_multilingual": False
+                })
+
+            self.assertEqual(
+                resp.json["errors"],
+                "Invalid data: email or badge number already in use"
+            )
+
+    def test_signup_dupe_badge_num(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/signup",
+                json={
+                    "badge_number":"1",
+                    "email":"u3@mail.com",
+                    "password":"password",
+                    "first_name":"duplicate badge",
+                    "last_name":"test",
+                    "dob":"2000-01-01 00:00:00",
+                    "gender":"Prefer not to say",
+                    "address":"1 Cherry lane",
+                    "city":"New York",
+                    "state":"NY",
+                    "zip_code":"11001",
+                    "phone_number":"9991234567",
+                    "is_student": True,
+                    "is_multilingual": False
+                })
+
+            self.assertEqual(
+                resp.json["errors"],
+                "Invalid data: email or badge number already in use"
+            )
+
+    def test_login(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/login",
+                json={
+                    "email": "u1@mail.com",
+                    "password": "password",
+                })
+
+            self.assertIsInstance(resp.json["token"], str)
+
+    def test_login_wrong_password(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/login",
+                json={
+                    "email": "u1@mail.com",
+                    "password": "badpassword",
+                })
+
+            self.assertEqual(resp.json["errors"], "Invalid credentials")
+
+    def test_login_wrong_password_email(self):
+        with self.client as c:
+            resp = c.post(
+                "/auth/login",
+                json={
+                    "email": "bad@email.com",
+                    "password": "badpassword",
+                })
+
+            self.assertEqual(resp.json["errors"], "Invalid credentials")
